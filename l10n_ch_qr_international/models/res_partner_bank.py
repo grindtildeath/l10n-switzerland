cbfdb436b0b96b40ea19ee2d466709c2bf509181
Akim Juillerat <akim.juillerat@camptocamp.com>
2026-09-10 20:33:58 +0200

    fixup! Add module l10n_ch_qr_international

--- l10n_ch_qr_international/models/res_partner_bank.py
+++ l10n_ch_qr_international/models/res_partner_bank.py
@@ -7,25 +7,25 @@ class ResPartnerBank(models.Model):
     _inherit = "res.partner.bank"
 
     def _get_error_messages_for_qr(self, qr_method, debtor_partner, currency):
+        # Copy of internal function from
+        #  l10n_ch.models.res_bank.py::ResPartnerBank._get_error_messages_for_qr
+        #  to remove the check on debtor_partner's country
+        # TODO: When migrating to v18.0, we can override _l10n_ch_qr_debtor_check
+        #  instead
+        def _get_error_for_ch_qr():
+            error_messages = [_("The Swiss QR code could not be generated for the following reason(s):")]
+            if self.acc_type != 'iban':
+                error_messages.append(_("The account type isn't QR-IBAN or IBAN."))
+            if currency.id not in (self.env.ref('base.EUR').id, self.env.ref('base.CHF').id):
+                error_messages.append(_("The currency isn't EUR nor CHF."))
+            return '\r\n'.join(error_messages) if len(error_messages) > 1 else None
+
         res = super()._get_error_messages_for_qr(qr_method, debtor_partner, currency)
         if (
             res
             and qr_method == "ch_qr"
-            and debtor_partner.country_id.code not in ("CH", "LI")
             and debtor_partner.commercial_partner_id.force_swiss_qr_invoice
+            and not _get_error_for_ch_qr()
         ):
-            # As super function is returning all the errors reasons in a single string
-            #  we need to split that string using what's used to join the reasons.
-            # Then we can check if there's any other error than the one for partners
-            #  not located in Switzerland or Liechtenstein
-            # If we have any other error, return the result from super call instead
-            #  of trying to remove string that was already translated, with a note
-            #  about what this module already allows
-            qr_errors_nbr = len(res.split("\r\n")) - 2
-            if qr_errors_nbr > 0:
-                return res + _(
-                    "\r\nNote: limitation on partner not in Switzerland can be ignored"
-                    " as partner is already configured to receive QR invoice."
-                )
             return None
         return res
